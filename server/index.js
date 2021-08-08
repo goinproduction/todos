7819343eaeb38d5ccc44d287d098ddf72f5ebd9c
viincyy <viincyy126@gmail.com>
2021-08-09 02:10:55 +0700

    configure backend mvc

--- server/index.js
+++ server/index.js
@@ -1,8 +1,36 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tymzp.mongodb.net/mern-app?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true,
+            }
+        );
+        console.log('Connected');
+    } catch (error) {
+        s;
+        console.log(error.message);
+        process.exit(1);
+    }
+};
+
+connectDB();
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 
 const PORT = 5000;
 
---
+++ server/routes/auth.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+router.get('/', (req, res) => res.send('USER TEST'));
+
+// @route POST /api/auth/register
+// @desc Register user
+// @access public
+
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Simple validation
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Missing username/ password' });
+    }
+
+    try {
+        // Check for existing user
+        const user = await User.findOne({ username });
+
+        if (user)
+            return res
+                .status(400)
+                .json({ success: false, message: 'User already exists' });
+
+        // All good
+        const hashedPassword = await argon2.hash(password);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+
+        // Return token
+        const accessToken = jwt.sign(
+            { userId: newUser._id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
+
+        res.json({
+            success: true,
+            message: 'User created successfully',
+            accessToken,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: 'false',
+            message: 'Internal server error',
+        });
+    }
+});
+
+// @route POST /api/auth/login
+// @desc Login user
+// @access public
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Simple validation
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Missing username/ password' });
+    }
+
+    try {
+        // Check existing user
+        const user = await User.findOne({ username });
+        if (!user)
+            return res.status(400).json({
+                success: false,
+                messsage: 'Incorrect username or password',
+            });
+
+        // Username exists
+        const validPassword = argon2.verify(user.password, password);
+        if (!validPassword)
+            return res.status(400).json({
+                success: false,
+                messsage: 'Incorrect username or password',
+            });
+
+        // All good
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
+        res.json({ success: true, message: 'Login successfully', accessToken });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: 'false',
+            message: 'Internal server error',
+        });
+    }
+});
+
+module.exports = router;
