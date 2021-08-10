58f226d02dbbfbe4a1358ae2bd2195c477b1e8e8
viincyy <viincyy126@gmail.com>
2021-08-10 18:16:12 +0700

    delete subfolder

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 
@@ -16,7 +17,8 @@ const connectDB = async () => {
                 useCreateIndex: true,
             }
         );
-        console.log('Connected');
+
+        console.log('MongoDB connected');
     } catch (error) {
         console.log(error.message);
         process.exit(1);
@@ -28,9 +30,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postRouter);
 
-const PORT = process.env.POST || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
