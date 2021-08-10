bcd1a04c2e4f70628ef43c2034e0ca63599864bd
viincyy <viincyy126@gmail.com>
2021-08-10 15:31:44 +0700

    upload project file

--- server/index.js
+++ server/index.js
@@ -32,6 +32,6 @@ app.use(cors());
 app.use('/api/auth', authRouter);
 app.use('/api/posts', postRouter);
 
-const PORT = 5000;
+const PORT = process.env.POST || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
