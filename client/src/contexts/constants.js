5e0de666229653a2d9ee936c3ac21e6c4b24b4b1
viincyy <viincyy126@gmail.com>
2021-08-10 16:07:14 +0700

    upload project file

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
-        ? 'http://localhost:5000/api'
+        ? 'https://powerful-stream-83838.herokuapp.com/api'
         : 'deployURL';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'mern-app';
