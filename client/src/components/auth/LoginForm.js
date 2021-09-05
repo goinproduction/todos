0f4efac76af0a3e64600ad6e7ac4d6bd4cd4bc1b
viincyy <viincyy126@gmail.com>
2021-09-05 20:29:16 +0700

    Validate login form using Formik/ Yup

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import AlertMessage from '../layouts/AlertMessage';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const LoginForm = () => {
     // Context
@@ -18,8 +20,17 @@ const LoginForm = () => {
     const [alert, setAlert] = useState(null);
 
     // destructure from local state
-    const { username, password } = loginForm;
-
+    // const { username, password } = loginForm;
+    // Login schema
+    const LoginSchema = Yup.object().shape({
+        username: Yup.string()
+            .email('Địa chỉ email không hợp lệ')
+            .required('Vui lòng nhập email'),
+        password: Yup.string()
+            .min(4, 'Mật khẩu phải từ 4 đến 60 kí tự')
+            .max(60, 'Mật khẩu phải từ 4 đến 60 kí tự')
+            .required('Vui lòng nhập mật khẩu'),
+    });
     const onChangeLoginForm = (event) =>
         setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
 
@@ -39,42 +50,48 @@ const LoginForm = () => {
         }
     };
     return (
-        <>
-            <Form className="my-3" onSubmit={login}>
-                <AlertMessage info={alert} />
-                <Form.Group className="my-3">
-                    <Form.Control
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        required
-                        value={username}
-                        onChange={onChangeLoginForm}
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        required
-                        value={password}
-                        onChange={onChangeLoginForm}
-                    />
-                </Form.Group>
-                <Button variant="success" type="submit" className="my-3">
-                    Login
-                </Button>
-            </Form>
-            <p>
-                Don't have an account?
-                <Link to="/register">
-                    <Button variant="info" size="sm" className="ms-2">
-                        Register
-                    </Button>
-                </Link>
-            </p>
-        </>
+        <Formik
+            initialValues={{ username: '', password: '' }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => setLoginForm(values)}
+        >
+            {({ errors, touched }) => (
+                <>
+                    <Form>
+                        <AlertMessage info={alert} />
+                        <Field
+                            name='username'
+                            placeholder='Email'
+                            className='input-style my-1'
+                            type='email'
+                        />
+                        {errors.username && touched.username ? (
+                            <p className='error-message'>{errors.username}</p>
+                        ) : null}
+                        <Field
+                            name='password'
+                            placeholder='Password'
+                            className='input-style mb-1 mt-2'
+                            type='password'
+                        />
+                        {errors.password && touched.password ? (
+                            <p className='error-message'>{errors.password}</p>
+                        ) : null}
+                        <button type='submit' className='button-style'>
+                            Đăng nhập
+                        </button>
+                    </Form>
+                    <p className='mt-3'>
+                        Bạn đã có tài khoản?
+                        <Link to='/register'>
+                            <Button variant='info' size='sm' className='ms-2'>
+                                Đăng ký
+                            </Button>
+                        </Link>
+                    </p>
+                </>
+            )}
+        </Formik>
     );
 };
 
